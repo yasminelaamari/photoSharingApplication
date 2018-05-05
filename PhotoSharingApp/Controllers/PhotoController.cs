bd0b0e6a9ef5e4a9807f0e6cc3185f47dd0483b4
yasminelaamari <38991915+yasminelaamari@users.noreply.github.com>
2018-05-05 13:43:16 +0200

    adding Controllers folder

---
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class PhotoController : System.Web.Mvc.Controller 
+    {
+        // GET: Photo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
