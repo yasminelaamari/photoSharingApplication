2758f53087e44aacacb7ca21d5633b85b95651c1
yasminelaamari <38991915+yasminelaamari@users.noreply.github.com>
2018-05-18 02:50:00 +0200

    adding valueReporter

---
+++ PhotoSharingApp/Controllers/ValueReporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class ValueReporter : ActionFilterAttribute
+    {
+        private void logValues(RouteData routeData)
+        {
+            Debug.WriteLine("Action Values");
+            foreach (var value in routeData.Values)
+            {
+                Debug.WriteLine("Key : " + value.Key + " / Value :" + value.Value);
+            }
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            logValues(filterContext.RouteData);
+
+        }
+    }
+}
